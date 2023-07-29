68b891a7e6bf76bc5243d4d905be4b6641d5ac3d
AmogelangKorase <2544404@students.wits.ac.za>
2023-07-29 15:18:29 +0000

    ammendments to the donor sign in

--- app/src/main/java/com/example/edugive/SignUp.java
+++ app/src/main/java/com/example/edugive/SignUp.java
@@ -2,13 +2,26 @@ package com.example.edugive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
+    Button reg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        reg = findViewById(R.id.button2);
+
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(SignUp.this, MainActivity.class);
+            }
+        });
     }
 }
